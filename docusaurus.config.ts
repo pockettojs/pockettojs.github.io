e6cd3af21ee101a33375474a6fbeb3da04d22bd8
Kent Ng <kent.ng201@gmail.com>
2024-10-23 22:26:22 +0800

    add local search mechanism

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -30,6 +30,12 @@ const config: Config = {
     locales: ['en'],
   },
 
+  plugins: [
+    [require.resolve('docusaurus-lunr-search'), {
+      languages: ['en'] // language codes
+    }],
+  ],
+
   presets: [
     [
       'classic',
@@ -64,12 +70,12 @@ const config: Config = {
   ],
 
   themeConfig: {
-    algolia: {
-      appId: '9B9A6KT33V',
-      apiKey: '6ffdc6b54e0faaa87a8e6586b306b6f4',
-      indexName: 'pocketto_docs',
-      contextualSearch: true,
-    },
+    // algolia: {
+    //   appId: '9B9A6KT33V',
+    //   apiKey: '6ffdc6b54e0faaa87a8e6586b306b6f4',
+    //   indexName: 'pocketto_docs',
+    //   contextualSearch: true,
+    // },
     search: {
       placeholder: 'Search...',
     },
